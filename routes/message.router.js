00a2fc12f714bdb14223705afd9770255b187807
Timo Blumer <timo.blumer@gewa.ch>
2024-05-24 01:23:43 +0200

    udfjwlfj

--- routes/message.router.js
+++
@@ -1,8 +0,0 @@
-// routes/message.router.js
-const express = require('express');
-const router = express.Router();
-const messageController = require('../controller/message.controller');
-
-router.get("/:ticketId", messageController.getByTicketId);
-
-module.exports = router;
